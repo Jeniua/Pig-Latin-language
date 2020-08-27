4b008419d29a7c7e0f28b834f3bf61672523bfc6
Jeniua <64768209+Jeniua@users.noreply.github.com>
2020-08-27 14:15:17 +0300

    Update Ex1.py
    Add while loop

--- Ex1.py
+++ Ex1.py
@@ -1,9 +1,12 @@
 def pig_latin():
-    word = input('Enter word for translate: ')
-    if word[0] in 'aeiou':
-        return word + 'way'
+    while True:
+        word = input('Enter word for translate: ')
+        if not word:
+            print('End')
+            break
+        if word[0] in 'aeiou':
+            print(word + 'way')
+        else:
+            print(f'{word[1:]}{word[0]}ay')
 
-    else:
-        return f'{word[1:]}{word[0]}ay'
-
-print(pig_latin())
+pig_latin()
