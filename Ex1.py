6642105b2b858de675a1fa0c6fa39acc76b7b4f5
Jeniua <64768209+Jeniua@users.noreply.github.com>
2020-08-27 14:39:03 +0300

    Update Ex1.py
    Added ability to translate whole sentences

--- Ex1.py
+++ Ex1.py
@@ -1,12 +1,12 @@
-def pig_latin():
-    while True:
-        word = input('Enter word for translate: ')
-        if not word:
-            print('End')
-            break
-        if word[0] in 'aeiou':
-            print(word + 'way')
-        else:
-            print(f'{word[1:]}{word[0]}ay')
-
-pig_latin()
+while True:
+    sentence = input('Eneter sentence to translate: ')
+    if not sentence:
+        print('Bye bye')
+        break
+    new = ''
+    for element in sentence.split():
+            if element[0] in 'aeiou':
+                new += element + 'way '
+            else:
+                new += element[1:] + element[0] + 'ay '
+    print(new)
